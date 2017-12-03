1ff50db2f14f6d47ad4723b323b602dfb8f3bb9b
Yu-Ann Chen <red8012@gmail.com>
2017-12-03 21:20:28 +0800

    get current dir

--- root.sh
+++ root.sh
@@ -1,1 +1,12 @@
-echo 'hi'
+#!/bin/bash
+set -e
+echo 'Welcome to ServerSetupScript'
+
+function getCurrentDir() {
+    local current_dir="${BASH_SOURCE%/*}"
+    if [[ ! -d "${current_dir}" ]]; then current_dir="$PWD"; fi
+    echo "${current_dir}"
+}
+
+current_dir=$(getCurrentDir)
+source "${current_dir}/setupLibrary.sh"
