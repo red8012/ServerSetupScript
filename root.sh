1b54500a809f5baa3a53e40161ef1bd35511ad28
Yu-Ann Chen <red8012@gmail.com>
2017-12-03 21:34:01 +0800

    prompt for password

--- root.sh
+++ root.sh
@@ -8,6 +8,22 @@ function getCurrentDir() {
     echo "${current_dir}"
 }
 
+function promptForPassword() {
+   PASSWORDS_MATCH=0
+   while [ "${PASSWORDS_MATCH}" -eq "0" ]; do
+       read -s -rp "Enter new UNIX password:" password
+       printf "\n"
+       read -s -rp "Retype new UNIX password:" password_confirmation
+       printf "\n"
+
+       if [[ "${password}" != "${password_confirmation}" ]]; then
+           echo "Passwords do not match! Please try again."
+       else
+           PASSWORDS_MATCH=1
+       fi
+   done
+}
+
 current_dir=$(getCurrentDir)
 source "${current_dir}/setupLibrary.sh"
 
