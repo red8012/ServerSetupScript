d0e9f97fffbf75fb035a84d9d93a4ed65fef5182
Yu-Ann Chen <red8012@gmail.com>
2017-12-03 21:44:31 +0800

    remove lib dependency

--- root.sh
+++ root.sh
@@ -24,13 +24,68 @@ function promptForPassword() {
    done
 }
 
+# Add the new user account
+# Arguments:
+#   Account Username
+#   Account Password
+#   Flag to determine if user account is added silently. (With / Without GECOS prompt)
+function addUserAccount() {
+    local username=${1}
+    local password=${2}
+    local silent_mode=${3}
+
+    if [[ ${silent_mode} == "true" ]]; then
+        sudo adduser --disabled-password --gecos '' "${username}"
+    else
+        sudo adduser --disabled-password "${username}"
+    fi
+
+    echo "${username}:${password}" | sudo chpasswd
+    sudo usermod -aG sudo "${username}"
+}
+
+# Execute a command as a certain user
+# Arguments:
+#   Account Username
+#   Command to be executed
+function execAsUser() {
+    local username=${1}
+    local exec_command=${2}
+
+    sudo -u "${username}" -H bash -c "${exec_command}"
+}
+
+# Add the local machine public SSH Key for the new user account
+# Arguments:
+#   Account Username
+#   Public SSH Key
+function addSSHKey() {
+    local username=${1}
+    local sshKey=${2}
+
+    execAsUser "${username}" "mkdir -p ~/.ssh; chmod 700 ~/.ssh; touch ~/.ssh/authorized_keys"
+    execAsUser "${username}" "echo \"${sshKey}\" | sudo tee -a ~/.ssh/authorized_keys"
+    execAsUser "${username}" "chmod 600 ~/.ssh/authorized_keys"
+}
+
+# Disables the sudo password prompt for a user account by editing /etc/sudoers
+# Arguments:
+#   Account username
+function disableSudoPassword() {
+    local username="${1}"
+    sudo cp /etc/sudoers /etc/sudoers.bak
+    sudo bash -c "echo '${1} ALL=(ALL) NOPASSWD: ALL' | (EDITOR='tee -a' visudo)"
+}
+
 current_dir=$(getCurrentDir)
-source "${current_dir}/setupLibrary.sh"
 
-
+# setup user
 username="user"
 promptForPassword
-addUserAccount "${username}" "${password}"
+addUserAccount "${username}" "${password}" "true"
+read -rp $'Paste in the public SSH key for the new user:\n' sshKey
+addSSHKey "${username}" "${sshKey}"
 usermod -aG sudo user
 disableSudoPassword "${username}"
-configureNTP
+sudo apt-get update
+sudo apt-get --assume-yes install ntp
